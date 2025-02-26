b78dff17dae0bd47f480180488077323627a3fca
JamesH11-FML <jhotchkiss11@fingerlakes.edu>
2025-02-26 14:23:57 -0500

    Add files via upload

---
+++ james_hotchkiss-inches_to_feet.py
@@ -0,0 +1,76 @@
+# FILE NAME - inches_to_feet.py
+
+# NAME: James Hotchkiss
+# DATE: 02/25/2025
+# BRIEF DESCRIPTION:  inchest to feet conversion
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+
+
+# BEWARE! You'll need to cast not only the input from the user
+# but also maybe the number of feet
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+inches = int(input('Provide inches: '))
+feet = int(inches/12)
+remainder = int(inches % 12)
+print(f'{inches} inches is {feet} feet, and {remainder} inches.')
+    
+    
+    
+    
+    
+    
+    
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+Enter the number of inches: 14
+
+14 inches is 1 feet, and 2 inches
+
+
+'''
+
+
+
+'''
+
+Enter the number of inches: 100
+
+100 inches is 8 feet, and 4 inches
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. What does it mean to "cast" input from the user?
+
+
+
+
+
+'''
